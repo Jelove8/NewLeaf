9bc245b82f62caf10549b78ecacf0b3f2ae53da4
Anjelo Valera <jelovalera@gmail.com>
2022-03-12 12:00:56 -0500

    vzsdbaWD

---
+++ app/src/main/java/com/example/newleaf2022/NumpadFragment.kt
@@ -0,0 +1,31 @@
+package com.example.newleaf2022
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newleaf2022.databinding.FragmentNumpadBinding
+
+
+class NumpadFragment : Fragment() {
+
+    private var fragmentBinding: FragmentNumpadBinding? = null
+    private val binding get() = fragmentBinding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentBinding = FragmentNumpadBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fragmentBinding = null
+    }
+
+
+}
